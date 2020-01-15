bdf8642f581d9833dfdd112b36449610f76b6e5c
min950903 <min950903@gmail.com>
2020-01-15 11:23:09 -0800

    ReviewController add

---
+++ TTIPApplication/TTIPApplication/Controllers/ReviewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TTIPApplication.Controllers
+{
+    public class ReviewController : Controller
+    {
+        // GET: Review
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
