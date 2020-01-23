8fc0d2acead4bf409671b69dd5adf89a62d50c15
지훈 김 <rlawlgns9412@gmail.com>
2020-01-24 04:19:46 +0900

    HomePage 2020-01-24 11:19

--- TTIPApplication/TTIPApplication/Controllers/HomeController.cs
+++ TTIPApplication/TTIPApplication/Controllers/HomeController.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TTIPApplication.Models;
 
 namespace TTIPApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private TTIP_DBEntities db = new TTIP_DBEntities();
+
         public ActionResult Index()
         {
-            return View();
+            var Home = db.PLACE.Include(p => p.CATEGORY1);
+            return View(Home);
         }
 
         public ActionResult About()
